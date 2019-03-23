b48de71098c8af07f84b36b82621a3a6705e748b
Tyler Evans <ty.evans001@gmail.com>
2019-03-23 13:25:31 -0500

    Add specular highlights

--- src/raytracer.cpp
+++ src/raytracer.cpp
@@ -6,13 +6,16 @@
 #include <fstream>
 #include <string>
 #include <glm/glm.hpp>
+#include <algorithm>
 #include <glm/gtx/string_cast.hpp>
+
 
 #include "json.hpp"
 
 using json = nlohmann::json;
 
 const char *PATH = "scenes/";
+const float MAX_T = 1e6;
 
 double fov = 60;
 colour3 background_colour(0, 0, 0);
@@ -69,21 +72,15 @@ void choose_scene(char const *fn) {
 }
 
 
-float plane_intersection(point3 e, point3 d, point3 n, point3 a) {
-	float t = glm::dot(n, a - e) / glm::dot(n, d);
-	if (t > 0.0)
-		return t;
-	else
-		return 1e6;
+/************************************************************************************************/
+
+float plane_intersection(point3 eye, point3 direction, point3 plane_normal, point3 plane_point) {
+	float t = glm::dot(plane_normal, plane_point - eye) / glm::dot(plane_normal, direction);
+	return t > 0.0? t : MAX_T;
 }
 
-float triangle_intersetion(point3 e, point3 d, std::vector<std::vector<float>> triangle) {
-	point3 a = vector_to_vec3(triangle[0]);
-	point3 b = vector_to_vec3(triangle[1]);
-	point3 c = vector_to_vec3(triangle[2]);
+float triangle_intersetion(point3 e, point3 d, point3 a, point3 b, point3 c, point3 plane_n, std::vector<std::vector<float>> triangle) {
 
-	point3 plane_n = glm::cross(b - a, c - a);
-	plane_n = glm::normalize(plane_n);
 	float t = glm::dot(plane_n, a - e) / glm::dot(plane_n, d);
 
 	point3 x = e + t * d;
@@ -92,10 +89,7 @@ float triangle_intersetion(point3 e, point3 d, std::vector<std::vector<float>> t
 	bool triangle_test_1 = glm::dot(glm::cross(c - b, x - b), plane_n) > 0.0;
 	bool triangle_test_2 = glm::dot(glm::cross(a - c, x - c), plane_n) > 0.0;
 
-	if (triangle_test_0 && triangle_test_1 && triangle_test_2)
-		return t;
-	else
-		return 1e6;
+	return triangle_test_0 && triangle_test_1 && triangle_test_2 ? t : MAX_T;
 }
 
 float sphere_intersection(point3 e, point3 d, point3 center, float r) {
@@ -111,26 +105,65 @@ float sphere_intersection(point3 e, point3 d, point3 center, float r) {
 		float t = glm::min(solution_0, solution_1);
 		return t;
 	}
-	return 1e6;
+	return MAX_T;
 }
 
 
-colour3 calculate_lighting(point3 normal, point3 x, colour3 ambient, colour3 diffuse, colour3 specular, float shininess) {
+float calculate_I_d(point3 N, point3 L) {
+	float I_d = glm::dot(glm::normalize(N), glm::normalize(L));
+	return std::max(I_d, 0.0f);
+}
 
-	float id = 1.0;
-	float kd = 1.0;
+float calculate_I_s(point3 N, point3 L, point3 V, float alpha) {
+	N = glm::normalize(N);
+	L = glm::normalize(L);
+	V = glm::normalize(V);
 
-	point3 light_source = glm::vec3(-1.0, 8.0, -5.0);
-	float I_d = glm::dot(glm::normalize(normal), glm::normalize(light_source - x));
+	point3 R = (2.0f * glm::dot(N, L) * N) - L;
+	R = glm::normalize(R);
 
-	return diffuse * I_d * colour3(0.8, 0.8, 0.0);
+	float base = std::min(std::max(glm::dot(R, V), 0.0f), 1.0f);
+	float I_s = std::pow(base, alpha);
+	return I_s;
+}
 
+colour3 calculate_lighting(point3 normal, point3 x, point3 look, json &material, json &lights) {
+
+	colour3 ambient = material.find("ambient") != material.end() ? vector_to_vec3(material["ambient"]) : colour3(0.0, 0.0, 0.0);
+	colour3 diffuse = material.find("diffuse") != material.end() ? vector_to_vec3(material["diffuse"]) : colour3(0.0, 0.0, 0.0);
+	colour3 specular = material.find("specular") != material.end() ? vector_to_vec3(material["specular"]) : colour3(0.0, 0.0, 0.0);
+	float shininess = material.find("shininess") != material.end() ? float(material["shininess"]) : 0.0;
+
+	colour3 total_colour = colour3(0.0, 0.0, 0.0);
+
+	for (json::iterator it = lights.begin(); it != lights.end(); ++it) {
+		json &light = *it;
+		colour3 light_colour = vector_to_vec3(light["color"]);
+
+		if (light["type"] == "ambient") {
+			total_colour += light_colour * ambient;
+		}
+		else if (light["type"] == "directional") {
+			point3 light_direction = vector_to_vec3(light["direction"]);
+			float I_d = calculate_I_d(normal, -light_direction);
+			float I_s = calculate_I_s(normal, -light_direction, look, shininess);
+			total_colour += light_colour * (I_d * diffuse + I_s * specular);
+		}
+		else if (light["type"] == "point") {
+			point3 light_position = vector_to_vec3(light["position"]);
+			float I_d = calculate_I_d(normal, light_position - x);
+			float I_s = calculate_I_s(normal, light_position - x, look, shininess);
+			total_colour += light_colour * (I_d * diffuse + I_s * specular);
+		}
+	}
+
+	return total_colour;
 }
 
 
 bool trace(const point3 &e, const point3 &s, colour3 &colour, bool pick) {
 
-	float min_t = 1e6;
+	float min_t = MAX_T;
 	json closest_object;
 	std::vector<std::vector<float>> closest_triangle; //if any
 	colour3 closest_colour;
@@ -140,16 +173,13 @@ bool trace(const point3 &e, const point3 &s, colour3 &colour, bool pick) {
 	d = glm::normalize(d);
 	// traverse the objects
 	json &objects = scene["objects"];
+	json &lights = scene["lights"];
+
 	for (json::iterator it = objects.begin(); it != objects.end(); ++it) {
 		json &object = *it;
 
 		// Every object will have a material
 		json &material = object["material"];
-		colour3 ambient = material.find("ambient") != material.end() ? vector_to_vec3(material["ambient"]) : colour3(0.0, 0.0, 0.0);
-		colour3 diffuse = material.find("diffuse") != material.end() ? vector_to_vec3(material["diffuse"]) : colour3(0.0, 0.0, 0.0);
-		colour3 specular = material.find("specular") != material.end() ? vector_to_vec3(material["specular"]) : colour3(0.0, 0.0, 0.0);
-		float shininess = material.find("shininess") != material.end() ? float(material["shininess"]) : 0.0;
-
 
 		if (object["type"] == "mesh") {
 			std::vector<std::vector<std::vector<float>>> triangles = object["triangles"];
@@ -158,16 +188,26 @@ bool trace(const point3 &e, const point3 &s, colour3 &colour, bool pick) {
 			for (std::vector<std::vector<std::vector<float>>>::size_type i = 0; i != triangles.size(); i++) {
 				std::vector<std::vector<float>> triangle = triangles[i];
 
-				float t = triangle_intersetion(e, d, triangle);
+				point3 a = vector_to_vec3(triangle[0]);
+				point3 b = vector_to_vec3(triangle[1]);
+				point3 c = vector_to_vec3(triangle[2]);
+
+				point3 plane_n = glm::cross(b - a, c - a);
+				plane_n = glm::normalize(plane_n);
+
+				float t = triangle_intersetion(e, d, a, b, c, plane_n, triangle);
+				point3 x = e + t * d;
 
 				if (t < min_t) {
 					min_t = t;
 					closest_object = object;
 					closest_triangle = triangle;
-					closest_colour = colour3(1.0, 0.0, 0.0);
+					//closest_colour = colour3(1.0, 0.0, 0.0);
+					closest_colour = calculate_lighting(plane_n, x, e - x, material, lights) + calculate_lighting(-plane_n, x, e - x, material, lights);
 					//colour = colour3(1.0, 0.0, 0.0);
 					//return true;
 					found = true;
+					break;
 				}
 			}
 
@@ -182,8 +222,7 @@ bool trace(const point3 &e, const point3 &s, colour3 &colour, bool pick) {
 				min_t = t;
 				closest_object = object;
 				point3 x = e + t * d;
-				colour = calculate_lighting(x - center, x, ambient, diffuse, specular, shininess);
-				closest_colour = colour;
+				closest_colour = calculate_lighting(x - center, x, e - x, material, lights);
 				//return true;
 				found = true;
 			}
@@ -194,11 +233,11 @@ bool trace(const point3 &e, const point3 &s, colour3 &colour, bool pick) {
 
 			//float t = glm::dot(n, a - e) / glm::dot(n, d);
 			float t = plane_intersection(e, d, n, a);
+			point3 x = e + t * d;
 
 			if (t < min_t) {
-				colour = calculate_lighting(n, e+t*d, ambient, diffuse, specular, shininess);
 				min_t = t;
-				closest_colour = colour;
+				closest_colour = calculate_lighting(n, x, e - x, material, lights);
 				//return true;
 				found = true;
 			}
